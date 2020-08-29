2ba7e9f090f472b1a6937f930269224d2d0d55ef
Elton <32944003+EltZ36@users.noreply.github.com>
2020-08-29 16:16:12 -0700

    Changed Shape
    Changed the shape to draw hexagons. I also added more to the color list in choices and I found a way to increase the speed of turtle beyond the turtle.speed(). It is pretty fast and if you want to see the turtle draw the shapes, you can comment those out.  There is also another shape option and you can remove the comment on line 18 to activate that.

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import turtle, random
-colors = ["black","red","green","blue","yellow","orange","purple","pink","gray"] 
+colors = ["black","red","green","blue","yellow","orange","purple","pink","gray","cyan","purple", "lightgreen","dark turquoise"] 
 tim = turtle.Turtle()
-def shape(t,space):
-  tim.width(5)
-  tim.speed(100)
-  for i in range(0,20):
+turtle.mainloop()
+turtle.tracer(100,100)
+def shape(t,size):
+  tim.speed(0)
+  #Change this for how many lines you want drawn.
+  for i in range(0,6):
+    '''
+    Use these for more color than just black.
     colors2 = random.choice(colors)
     tim.color(colors2)
-    tim.forward(space)
-    tim.right(50)
-    tim.backward(70)
-
+    '''
+    tim.forward(size/3)
+    tim.right(60)
+    #You can add this to create a different shape. When I set it to level 2 and size 100, it made a flower shape. At level 4 size 100, it became a very complex shape that was a mess of overlapping lines.
+    #tim.left(20)
+    
 def shape_recurse(t, level, size):
   if level == 1:
     shape(tim,150)
@@ -37,3 +43,4 @@ def Setup():
       print("Please type in the numbers again.")
 Setup()
 shape_recurse(tim, Question1, Question2)
+turtle.update()
