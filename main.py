197390183235e3943831ea2bd83266e4bc3248c7
Elton <32944003+EltZ36@users.noreply.github.com>
2020-08-28 18:27:52 -0700

    Created recursion function
    I created the recursion function and removed the code for making another shape in the shape function. When it is recursed, the repeating shape doesn't look too good.  I will most likely change shape and add user input to adjust the level and size of the shape.

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import turtle, random
 colors = ["black","red","green","blue","yellow","orange","purple","pink","gray"] 
-def shape():
-  tim = turtle.Turtle()
+tim = turtle.Turtle()
+def shape(t,space):
   tim.width(5)
   tim.speed(100)
   for i in range(0,20):
     colors2 = random.choice(colors)
     tim.color(colors2)
-    tim.forward(120)
+    tim.forward(space)
     tim.right(50)
     tim.backward(70)
-  tim.penup()
-  tim.goto(300,200)
-  tim.pendown()
-  for i in range(0,20):
-    colors2 = random.choice(colors)
-    tim.color(colors2)
-    tim.forward(120)
-    tim.right(50)
-    tim.backward(70)
-shape()
+
+def shape_recurse(t, level, size):
+  if level == 1:
+    shape(tim,150)
+  else:
+    for i in range (0,3):
+      shape_recurse(tim, level-1,size/10)
+      tim.right(50)
+      tim.backward(70)
+shape_recurse(tim, 2, 500)
